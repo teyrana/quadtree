a2268beb802c896b7265e37b61c022d34c80be60
Daniel_M_Williams <equipoise@gmail.com>
2019-08-27 22:25:24 -0400

    [feat] implemented polygon class

---
+++ include/geometry/polygon.hpp
@@ -0,0 +1,84 @@
+// The MIT License 
+// (c) 2019 Daniel Williams
+
+#ifndef _GEOMETRY_POLYGON_HPP_
+#define _GEOMETRY_POLYGON_HPP_
+
+#include <memory>
+#include <random>
+#include <sstream>
+#include <string>
+#include <vector>
+
+#include "geometry/bounds.hpp"
+#include "geometry/point.hpp"
+
+using geometry::Bounds;
+using geometry::Point;
+
+namespace geometry {
+class Polygon {
+public:
+    Polygon();
+    Polygon(std::string new_name);
+
+    // clears the internal point vector
+    void clear();
+
+    bool contains(double x, double y) const;
+    
+    // Retrieves the precomputed center of the polygon:
+    // Currently, this is a naive, unweighted average of the polygon points.
+    // \return 2D x,y point of the polygon's center
+    const Bounds& get_bounds() const;
+
+
+    // Retrieves the precomputed center of the polygon:
+    // Currently, this is a naive, unweighted average of the polygon points.
+    // \return was the load successful? 
+    // \sidef
+    bool load(const bool as_inclusive, std::vector<Point> source);
+
+    // dumps the contains points to stderr
+    // \param title - text to print in the output header
+    // \param pts - set of points to dump
+    void write_yaml(std::ostream& sink, std::string indent="") const; 
+
+protected:
+    // if necessary, adds an extra point to the end of the polygon-point vector to wrap it back to the first point.
+    // this is not strictly operationally necessary, but it simplifies most of the algorithms that need to iterate over the points.
+    void enclose_polygon();
+
+    // \brief isRightHanded()
+    // calculates if the polygon is defined in a right-handed // CCW direction.
+    // runs in O(n).
+    //
+    // See Also: https://en.wikipedia.org/wiki/Shoelace_formula
+    //
+    // \returns true: polgyon is right-handed.  false: left-handed
+    bool is_right_handed() const;
+
+    void set_default();
+
+    void update_bounds();
+
+protected: // Configuration parameters
+    std::string name;             ///< Arbitrary string describing this polygon. For humans.
+    std::vector<Point> points;    ///< Main data store for this class.  Contains the vertices of the polygon
+    bool inclusive;               ///< flags  if this polygon excludes area or includes area
+    Bounds bounds;                ///< x,y bounds at the (approximate) center of the polygon
+
+private:
+    friend class PolygonTest_DefaultConfiguration_Test;
+    friend class PolygonTest_ExplicitConfigurationSquare_Test;
+    friend class PolygonTest_ExplicitConfigurationIrregular_Test;
+    
+    // friend class PolygonTests_TestHandedness_Test;
+    // friend class PolygonTests_InBoundingBoxByX_Test;
+    // friend class PolygonTests_InConvexBoundariesByX_Test;
+    // friend class PolygonTests_InConcaveBoundariesByX_Test;
+    // friend class PolygonTests_InConcaveBoundariesByY_Test;
+};
+
+} // namespace geometry
+#endif
---
+++ src/geometry/polygon.cpp
@@ -0,0 +1,145 @@
+#include <cfloat>
+#include <cmath>
+#include <cstddef>
+#include <cstdlib>
+#include <iterator>
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+
+#include "geometry/polygon.hpp"
+
+using std::cerr;
+using std::endl;
+using std::ostream;
+using std::string;
+using std::setw;
+
+using geometry::Point;
+using geometry::Polygon;
+
+//---------------------------------------------------------------
+// Constructor
+//
+Polygon::Polygon():
+    Polygon("<unnamed>")
+{}
+
+Polygon::Polygon(string _name):
+    name(_name),
+    inclusive(true)
+{
+    set_default();
+}
+
+bool Polygon::load(const bool as_inclusive, std::vector<Point> source){
+    // if the new polygon contains insufficient points, abort and clear.
+    if(4 > source.size()){
+        return false;
+    }
+
+    this->inclusive = as_inclusive;
+    points = std::move(source);
+    update_bounds();
+
+    // cerr << "====== ====== ====== " << endl;
+    // write_yaml(cerr, "    ");
+
+    enclose_polygon();
+    if(! is_right_handed()){
+        std::reverse(std::begin(points), std::end(points));
+    }
+
+    // cerr << "====== ====== ====== " << endl;
+    // write_yaml(cerr, "    ");
+
+    return true;
+}
+
+void Polygon::clear(){
+    points.clear();
+    bounds.clear();
+}
+
+void Polygon::enclose_polygon(){
+    // ensure that polygon loops back
+    const auto& first_point = points[0];
+    const auto& last_point = points[points.size()-1];
+
+    if( ! first_point.near(last_point)){
+        points.emplace_back(first_point);
+    }
+}
+
+const Bounds& Polygon::get_bounds() const {
+    return bounds;
+}
+
+bool Polygon::is_right_handed() const {
+    double sum = 0;
+
+    // NOTE THE RANGE!:
+    //   this needs to iterate over all doubles
+    for( uint i = 0; i < (points.size()-1); ++i ){
+        auto& p1 = points[i];
+        auto& p2 = points[i+1];
+        sum += (p1.x*p2.y) - (p1.y*p2.x);
+    }
+
+    // // The shoelace formula includes a divide-by-2 step; ... but we don't
+    // // care about the magnitude, just the sign.  So skip this step.
+    // sum /= 2;
+
+    if( 0 > sum ){
+        // left-handed / clockwise points
+        return false;
+    }else{
+        // right-handed / counter-clockwise points
+        return true;
+    }
+}
+
+ostream& operator<<(ostream& os, const Polygon& poly)
+{
+    poly.write_yaml(os);
+    return os;
+}
+
+#include <iostream>     // std::cout
+#include <algorithm>    // std::reverse
+#include <vector>       // std::vector
+
+void Polygon::set_default(){
+    points.clear();
+    points.emplace_back( 0, 0);
+    points.emplace_back( 1, 0);
+    points.emplace_back( 1, 1);
+    points.emplace_back( 0, 1);
+    update_bounds();
+}
+
+void Polygon::update_bounds() {
+    bounds.clear();
+    for( uint i = 0; i < points.size(); ++i ){
+        auto& p = points[i];
+        bounds.extend(p);
+    }
+}
+
+
+void Polygon::write_yaml(std::ostream& sink, string indent) const {
+    sink << indent << "name: " << name << '\n';
+    sink << indent << "inclusive: " << inclusive << '\n';
+
+    sink << indent << "points: \n";
+    for( uint i = 0; i < points.size(); ++i ){
+        auto& p = points[i];
+        sink << indent << "    - " << p.x << ", " << p.y << '\n';
+    }
+
+    sink << indent << "bounds: \n";
+    sink << indent << "    center: [" << bounds.center.x << ", " << bounds.center.y << "]\n";
+    sink << indent << "    x: [" << bounds.get_x_min() << ", " << bounds.get_x_max() << "]\n";
+    sink << indent << "    y: [" << bounds.get_y_min() << ", " << bounds.get_y_max() << "]\n";
+
+}
