9d03c7a33a52f5cf4048c4528885b559b55811fd
Daniel_M_Williams <equipoise@gmail.com>
2019-10-18 16:20:48 -0400

    [refactor] simplify generate_diamond function

--- src/terrain.cpp
+++ src/terrain.cpp
@@ -14,18 +14,16 @@ using nlohmann::json;
 
 namespace terrain {
 
-nlohmann::json generate_diamond( const double boundary_width,
-                                 const double diamond_width,
+nlohmann::json generate_diamond( const double width,
                                  const double desired_precision)
 {
-    const double center = boundary_width/2;
-
-    return {{"bounds",  {{"x", center}, {"y", center}, {"width", boundary_width}}},
+    const double width_2 = width /2;
+    return {{"bounds",  {{"x", width_2}, {"y", width_2}, {"width", width}}},
             {"precision", desired_precision},
-            {"allow", {{{center + diamond_width, center},
-                        {center                , center + diamond_width},
-                        {center - diamond_width, center},
-                        {center                , center - diamond_width}}}}};
+            {"allow", {{{  width, width_2},
+                        {width_2,   width},
+                        {      0, width_2},
+                        {width_2,       0}}}}};
 }
 
 }; // namespace terrain
